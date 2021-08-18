c992ace4e813990ea50b5d5c9fbae6376bf47805
zyddnys <zyddnys@outlook.com>
2021-08-17 21:05:58 -0400

    update build

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+NAME=thvote-user_manager
 export PKG_CONFIG_ALLOW_CROSS=1
 export OPENSSL_STATIC=true
 export OPENSSL_DIR=/musl
 cargo build --target x86_64-unknown-linux-musl --release
-docker build --no-cache -t thvote-user_manager .
-docker save -o thvote-user_manager.tar thvote-user_manager
+docker build --no-cache -t ${NAME} .
+docker save -o ${NAME}.tar ${NAME}
